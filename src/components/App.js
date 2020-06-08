b00f2cec189b692002f34ffc80b2c53237d53c5b
ThermoDev <michael.pegios@hotmail.com>
2020-06-09 04:04:41 +1000

    Integrating React with Redux - Post-Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostList from './PostList';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="ui container">
+                <PostList />
+            </div>
+        );
+    }
+}
+export default App;
