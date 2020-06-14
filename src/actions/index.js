d013fec8cbb018cb7824741c6cb17a15c5b8991c
ThermoDev <michael.pegios@hotmail.com>
2020-06-15 00:47:54 +1000

    Removing Multiple Calls to User

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,18 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    // Map over each of the posts, and get unique values of userId
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // Iterate over each of the ids, and call fetchUser Action Creator, then dispatch it.
+    userIds.forEach(id => dispatch(fetchUser(id)));
+
+    // Alternate way to perform same actions as above.
+    // _.chain.map(getState().posts).map('userId').uniq.forEach(id => dispatch(fetchUser(id))).value();
+    
+}
 
 export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
