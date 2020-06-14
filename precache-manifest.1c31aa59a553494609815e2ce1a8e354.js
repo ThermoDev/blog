57f8f323a0a50074e177a3e7b924de541b2b2417
ThermoDev <michael.pegios@hotmail.com>
2020-06-15 00:56:31 +1000

    Updates

---
+++ precache-manifest.1c31aa59a553494609815e2ce1a8e354.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c572f5150e249663a42f0134cf5696",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "ff9ec521f9ebc5bc1468",
+    "url": "/blog/static/js/2.f40720d6.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/blog/static/js/2.f40720d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee98c2c66d58fc998074",
+    "url": "/blog/static/js/main.002a1fde.chunk.js"
+  },
+  {
+    "revision": "18587eccc92f26eed8a3",
+    "url": "/blog/static/js/runtime-main.a5ebe4ff.js"
+  }
+]);
